529cd9b8228e20e4b05c858b134fec50f280f5ba
Bach Nguyen <42868347+BachKingg@users.noreply.github.com>
2022-10-04 10:05:20 +0700

    last update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -19,6 +19,9 @@ export class DashboardComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    //  returns the list of heroes [2,3,4,5]
   }
+
+  
 
 }
